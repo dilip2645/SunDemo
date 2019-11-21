4f21329503cf6b8e9ce51bcc9612a50b53139d22
Your Name <dilipr@STI.COM>
2019-11-21 13:48:43 +0530

    target

---
+++ target/generated-sources/annotations/com/mycompany/myapp/domain/Rightalign_.java
@@ -0,0 +1,18 @@
+package com.mycompany.myapp.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Rightalign.class)
+public abstract class Rightalign_ {
+
+	public static volatile SingularAttribute<Rightalign, String> sdf;
+	public static volatile SingularAttribute<Rightalign, Long> id;
+
+	public static final String SDF = "sdf";
+	public static final String ID = "id";
+
+}
+
